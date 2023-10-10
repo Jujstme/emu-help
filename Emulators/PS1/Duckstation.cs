c32b72d21432d2fae0e8ca4f1f320f304ccf838b
Jujstme <just.tribe@gmail.com>
2023-10-10 20:26:39 +0200

    Added new sigscan for DuckStation
    Due to a recent source change https://github.com/stenzek/duckstation/commit/56fc207af69601b702e4965494103f553909d3b1 , the sigscan that was currently used for DuckStation is no longer valid. A new sigscan has been added to support the latest release of said amulator.

--- Emulators/PS1/Duckstation.cs
+++ Emulators/PS1/Duckstation.cs
@@ -14,7 +14,11 @@ namespace LiveSplit.EMUHELP.PS1
             if (!game.Is64Bit())
                 throw new Exception();
 
-            base_addr = game.SafeSigScanOrThrow(new SigScanTarget(3, "48 89 0D ???????? B8") { OnFound = (p, s, addr) => addr + 0x4 + p.ReadValue<int>(addr) });
+            base_addr = game.SafeSigScan(new SigScanTarget(3, "48 89 ?? ?? ?? ?? ?? ?? FF FF 1F 00") { OnFound = (p, s, addr) => addr + 0x4 + p.ReadValue<int>(addr) });
+
+            if (base_addr.IsZero())
+                base_addr = game.SafeSigScanOrThrow(new SigScanTarget(3, "48 89 0D ???????? B8") { OnFound = (p, s, addr) => addr + 0x4 + p.ReadValue<int>(addr) });
+
             ram_base = game.ReadPointer(base_addr);
 
             Debugs.Info("  => Hooked to emulator: Duckstation");
