72d11309d62a8304591f8cd4178e2fb162a775f7
Jujstme <just.tribe@gmail.com>
2024-05-16 16:41:09 +0200

    Polish

--- Emulators/GBC/GSR_qt.cs
+++ Emulators/GBC/GSR_qt.cs
@@ -46,13 +46,9 @@ namespace LiveSplit.EMUHELP.GBC
             var success = Helper.Game.ReadPointer(base_addr - 0x10, out var addr);
 
             if (success)
-            {
                 wram_base = addr;
-                iohram_base = base_addr + 0x13FC;
-                return true;
-            }
-            else
-                return false;
+
+            return success;
         }
     }
 }
